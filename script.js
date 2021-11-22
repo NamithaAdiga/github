fc8f0914595a0edf17ff1f637aac60c3675203c9
Namitha Adiga <namithaadiga123@gmail.com>
2021-11-22 18:50:54 +0530

    Added demo.txt

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("name").innerHTML="john";
+document.getElementById("age").innerHTML="38";
+document.getElementById("email").innerHTML="john@guvi.in";
+document.getElementById("city").innerHTML="delhi";
+document.getElementById("company").innerHTML="guvi geek network";
+document.getElementById("contact").innerHTML="124456";
+
+
+let imgelement=document.createElement("img");
+imgelement.src=""
---
+++ script1.js
@@ -0,0 +1,5 @@
+let count=0;
+let wordcount=()=>{
+    let texteneteredlength=document.querySelector("#text-area").value.length;
+    document.querySelector("#wordcount").textContent=
+}
